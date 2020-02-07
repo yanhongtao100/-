74790429ca073fa47db257186d6b51b5c9dd9eaa
yanhongtao100 <602503398@qq.com>
2020-02-07 23:46:39 +0800

    更改

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
-    // 'no-console': off,
+    
     'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'off'
